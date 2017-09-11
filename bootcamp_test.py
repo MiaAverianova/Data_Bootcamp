41a053a7b58449dac86756b0a098cee119d8060f
MiaAverianova <ma3669@nyu.edu>
2017-09-11 16:32:48 -0400

    added files from class 2

---
+++ bootcamp_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+Test program for Data Bootcamp course at NYU Stern
+"""
+
+import sys 
+
+print('Welcome to Data Bootcamp!')
+print('Python version')
+print(sys.version)
+
+
